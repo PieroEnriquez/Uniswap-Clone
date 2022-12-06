8d553ba3989cab4dc71c76d2883d2ab2db448f57
Piero Enriquez <pieroenriquezvera@gmail.com>
2022-12-05 22:53:14 -0500

    Creating swap form

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header.js'
+import Main from '../components/Main.js'
 
 const style = {
   wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between`,
@@ -10,7 +11,7 @@ export default function Home() {
   return (
     <div className={style.wrapper}>
       <Header />
-      <h2>Main</h2>
+      <Main />
       <h2>Transaction History</h2>
     </div>
   )
